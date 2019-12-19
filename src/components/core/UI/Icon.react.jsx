3ca89b4369e4dbde89cf26fa8d10d67539bad06f
Dennis Tismenko <dtismenkodeveloper@gmail.com>
2019-12-19 15:41:23 -0500

    Modify icon fetching

--- src/components/core/UI/Icon.react.jsx
+++ src/components/core/UI/Icon.react.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import classes from './style/Icon.module.css';
-
-const iconStyles = {
-  correct: classes.Correct,
-  incorrect: classes.Incorrect,
-  help: classes.Help,
-  drag: classes.Drag
-};
+import icons from './util/icons';
 
 const Icon = ({size, type}) => {
-  const styleType = iconStyles[type];
   return (
     <div
-      className={[classes.Icon, styleType].join(' ')}
-      style={{width: `${size + (size/2)}rem`, height: `${size}rem`, backgroundSize: `${size}rem`}}
+      className={classes.Icon}
+      style={{
+        width: `${size + size / 2}rem`,
+        height: `${size}rem`,
+        backgroundSize: `${size}rem`,
+        backgroundImage: `url('${icons[type]}')`
+      }}
     ></div>
   );
 };
---
+++ src/components/core/UI/util/icons.js
@@ -0,0 +1,11 @@
+/**
+ * Add any icon imports to this file
+ */
+
+import incorrect from '../../../../assets/icons/incorrect.svg';
+import correct from '../../../../assets/icons/correct.svg';
+import gear from '../../../../assets/icons/gear.svg';
+import drag from '../../../../assets/icons/drag.svg';
+import help from '../../../../assets/icons/help.svg';
+
+export default {incorrect, correct, gear, drag, help};
